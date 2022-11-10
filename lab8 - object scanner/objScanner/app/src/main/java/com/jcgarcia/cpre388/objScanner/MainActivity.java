ddb1ed2d5e9f379ba7d94288a913a6ecaa812073
Jose Carlos Garcia <jcgarcia@iastate.edu>
2022-11-10 03:58:11 -0600

    added machine learning kit

--- lab8 - object scanner/objScanner/app/src/main/java/com/jcgarcia/cpre388/objScanner/MainActivity.java
+++ lab8 - object scanner/objScanner/app/src/main/java/com/jcgarcia/cpre388/objScanner/MainActivity.java
@@ -2,32 +2,65 @@ package com.jcgarcia.cpre388.objScanner;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.CameraX;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.media.Image;
 import android.os.Bundle;
+import android.util.Size;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.label.ImageLabel;
+import com.google.mlkit.vision.label.ImageLabeler;
+import com.google.mlkit.vision.label.ImageLabeling;
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
 
+import java.io.File;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
     private PreviewView previewView;
+    private Button buttond;
+    //private ImageLabeler labeler;
+    private  ImageCapture imageCapture;
+    private  ImageAnalysis imageAnalysis;
+    private Context context;
+    private TextView textView;
 
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         previewView = findViewById(R.id.previewView);
+
+    //    labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS);
+        context = MainActivity.this;
+        textView = findViewById(R.id.textView);
+        buttond = findViewById(R.id.button);
 
         cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         cameraProviderFuture.addListener(() -> {
@@ -39,6 +72,8 @@ public class MainActivity extends AppCompatActivity {
                 // This should never be reached.
             }
         }, ContextCompat.getMainExecutor(this));
+
+
     }
 
     void bindPreview(@NonNull ProcessCameraProvider cameraProvider) {
@@ -51,7 +86,84 @@ public class MainActivity extends AppCompatActivity {
 
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
+        imageCapture = new ImageCapture.Builder()
+                        .build();
+
+        View.OnClickListener click = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(new File(String.valueOf(System.currentTimeMillis()))).build();
+
+                imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(MainActivity.this),
+                        new ImageCapture.OnImageSavedCallback() {
+                            @Override
+                            public void onImageSaved(ImageCapture.OutputFileResults outputFileResults) {
+                                // insert your code here.
+                                String msg = "Image has been captured!" + outputFileResults.getSavedUri();
+                                Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
+                            }
+                            @Override
+                            public void onError(ImageCaptureException error) {
+                                String msg = "Pic capture failed ";
+                                Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+                            }
+                        }
+                );
+            }
+        };
+
+        buttond.setOnClickListener(click);
+
+        ImageLabelerOptions options = new ImageLabelerOptions.Builder()
+                .setConfidenceThreshold(0.7f)
+                .build();
+        ImageLabeler labeler = ImageLabeling.getClient(options);
+
+        imageAnalysis = new ImageAnalysis.Builder()
+                        // enable the following line if RGBA output is needed.
+                        //.setOutputImageFormat(ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                        .setTargetResolution(new Size(1280, 720))
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        .build();
+
+        imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), new ImageAnalysis.Analyzer() {
+                    @Override
+                    public void analyze(ImageProxy imageProxy) {
+                        @SuppressLint("UnsafeOptInUsageError") Image mediaImage = imageProxy.getImage();
+                        if (mediaImage != null) {
+                            InputImage image =
+                                    InputImage.fromMediaImage(mediaImage, imageProxy.getImageInfo().getRotationDegrees());
+                            // Pass image to an ML Kit Vision API
+                            labeler.process(image)
+                                    .addOnSuccessListener(new OnSuccessListener<List<ImageLabel>>() {
+                                        @Override
+                                        public void onSuccess(List<ImageLabel> labels) {
+                                            String display = "";
+                                            for (ImageLabel label : labels) {
+                                                String text = label.getText();
+                                                float confidence = label.getConfidence();
+                                                int index = label.getIndex();
+                                                display = display + String.format("#%d: %s with %f\n", index, text, confidence);
+                                            }
+
+                                        }
+                                    })
+                                    .addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            // Task failed with an exception
+                                            String msg = "It's not working bro ";
+                                            Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+                                        }
+                                    });
+                        }
+                        imageProxy.close();
+                    }
+                });
+
         Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview);
     }
+
+
 
 }
