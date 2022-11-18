b77c489cef5a62c2df704cd46f275df6d99f9ba5
Jose Carlos Garcia <jcgarcia@iastate.edu>
2022-11-18 01:44:22 -0600

    working counter

--- lab9 - pedometer/pedometer/app/src/main/java/com/jcgarcia/cpre388/pedometer/MainActivity.java
+++ lab9 - pedometer/pedometer/app/src/main/java/com/jcgarcia/cpre388/pedometer/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
+import androidx.core.os.HandlerCompat;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
@@ -15,30 +16,48 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager sensorManager;
     private Sensor built_in_sensor;
-    private TextView built_in;
+    private ExecutorService executor;
+    private Handler threadedHandler;
+    private Sensor accel;
+    private TextView actual;
     private TextView custom;
     private StepViewModel custom_viewModel;
+    private StepViewModel sensor_viewModel;
     private Long actual_steps = 0L;
-
+    private Long calc_steps = 0L;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        built_in = findViewById(R.id.builtin);
+        actual = findViewById(R.id.actualSensor);
         custom = findViewById(R.id.custom);
 
+        executor = Executors.newFixedThreadPool(2);
+        threadedHandler = HandlerCompat.createAsync(Looper.getMainLooper());
+
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        built_in_sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        built_in_sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
 
         //Custom Method of Counting Steps listener:
         custom_viewModel = new ViewModelProvider(this).get(StepViewModel.class);
+        sensor_viewModel = new ViewModelProvider(this).get(StepViewModel.class);
+        sensor_viewModel.release();
         custom_viewModel.release();
 
         final Observer<Long> stepObserver = new Observer<Long>(){
@@ -52,31 +71,61 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             @Override
             public void onChanged(@Nullable final Long steps){
                 String set = String.format("Steps: " + steps.toString());
-                built_in.setText(set);
+                actual.setText(set);
             }
         };
 
         custom_viewModel.getStepLiveData().observe(this, stepObserver);
-        custom_viewModel.getSensorLiveData().observe(this, sensorObserver);
+        sensor_viewModel.getStepLiveData().observe(this, sensorObserver);
     }
 
     @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
-            actual_steps++;
-            custom_viewModel.sen_storeData(actual_steps);
-            Toast.makeText(getBaseContext(), "HELLO WORLD", Toast.LENGTH_LONG).show();
+    protected void onResume() {
+        super.onResume();
+        sensorManager.registerListener(this, built_in_sensor, SensorManager.SENSOR_DELAY_UI);
+        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    public void onReset(View view){
+        actual.setText("STEPS RESET: 0 STEPS");
+        custom.setText("STEPS RESET: 0 STEPS");
+        custom_viewModel.release();
+        sensor_viewModel.release();
+        Toast.makeText(getBaseContext(), "Reset Steps", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        Sensor sensor = sensorEvent.sensor;
+        float[] values = sensorEvent.values;
+        int value = -1;
+
+        if (values.length > 0) {
+            value = (int) values[0];
         }
+        if (sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
+            Runnable step = new Runnable() {
+                @Override
+                public void run() {
+                    actual_steps = actual_steps + 1;
+                    sensor_viewModel.step_storeData(actual_steps);
+                }
+            };
+            executor.submit(step);
+        }
+
+        if(sensor.getType() == Sensor.TYPE_ACCELEROMETER){
+            Runnable acc = new Runnable() {
+                @Override
+                public void run() {
+
+                }
+            };
+            executor.submit(acc);
+        }
+
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {}
-
-
-    public void onReset(View view){
-        built_in.setText("0 Steps");
-        custom.setText("0 Steps");
-        custom_viewModel.release();
-    }
-
 }
--- lab9 - pedometer/pedometer/app/src/main/java/com/jcgarcia/cpre388/pedometer/StepViewModel.java
+++ lab9 - pedometer/pedometer/app/src/main/java/com/jcgarcia/cpre388/pedometer/StepViewModel.java
@@ -5,8 +5,6 @@ import androidx.lifecycle.ViewModel;
 
 public class StepViewModel extends ViewModel {
     private MutableLiveData<Long> stepLiveData = new MutableLiveData<>();
-    private MutableLiveData<Long> sensorLiveData = new MutableLiveData<>();
-
     private final static Long RESET_COUNTER = 0L;
 
     public StepViewModel(){}
@@ -14,20 +12,12 @@ public class StepViewModel extends ViewModel {
     public void step_storeData(Long steps){
         stepLiveData.setValue(steps);
     }
-    public void sen_storeData(Long steps){
-        sensorLiveData.setValue(steps);
-    }
 
     public void release(){
         stepLiveData.setValue(RESET_COUNTER);
-        sensorLiveData.setValue(RESET_COUNTER);
     }
 
     public MutableLiveData<Long> getStepLiveData(){
         return stepLiveData;
-    }
-
-    public MutableLiveData<Long> getSensorLiveData() {
-        return sensorLiveData;
     }
 }
